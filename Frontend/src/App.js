8ff403a5f9b7ff7e70183adc412b8a56a62b0831
Lemespien <Nilhogg@gmail.com>
2020-08-13 18:04:59 +0200

    Styled some cards and added basic page navigation

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiResponse: "",
+      posts: "",
+      title: '',
+      description: '',
+      content: '',
     }
   }
 
-  handleForm = (event) => {
+  handleFormChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState(
+      {
+        [name]: value,
+      }
+    )
+  }
+
+  handleForm = async (event) => {
     event.preventDefault();
-    console.log("YO");
+    const title = this.state.title;
+    const description = this.state.description;
+    const content = this.state.content;
+    await fetch("http://localhost:8080/post", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "title": title,
+        "description": description,
+        "content": content
+      }),
+    });
+    this.callAPI();
+  }
+
+  deletePost = async (id) => {
+    await fetch(`http://localhost:8080/post/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+    });
+    this.callAPI();
   }
 
   callAPI = async () => {
@@ -20,54 +59,50 @@ class App extends React.Component {
       const data = await fetch("http://localhost:8080/post");
       const dataText = await data.json();
       this.setState({
-        apiResponse: dataText,
+        posts: dataText,
       });
+      console.log("API was called")
+      console.log(this.state);
     } catch (error) {
       console.error(error);
     }
 
   }
+
+  pageHandler = (page) => {
+    console.log(page);
+    if (page === "Posts") {
+      this.callAPI();
+    }
+  }
   render() {
     let renderstuff = [];
-    if (this.state.apiResponse !== '') {
-      this.state.apiResponse.forEach(post => {
+    if (this.state.posts !== '') {
+      this.state.posts.forEach(post => {
+        const { _id, title, description, content } = post;
         renderstuff.push(<Post
-          post={post}
-          key={post._id}
+          key={_id}
+          id={_id}
+          title={title}
+          description={description}
+          content={content}
+          deletePost={this.deletePost}
         />);
       });
     }
-
 
     return (
       <div className="App" >
         <header className="App-header">
           <nav >
-            <ul style={{ display: "flex", listStyle: "none" }}>
-              <li style={{ padding: "15px" }}><a href="#posts" onClick={this.callAPI}>Posts</a>
-              </li>
-              <li style={{ padding: "15px" }}><a href="#about">About</a>
-              </li>
-              <li style={{ padding: "15px" }}><a href="#projects">Projects</a>
-              </li>
-            </ul>
+            <Navigation pageHandler={this.pageHandler} />
           </nav>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <PostForm handleForm={this.handleForm} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-
-            Learn React
-        </a>
         </header>
-        <section>
+
+        <section className="post-section">
+          <Post title="Hello world" description="This is a post" content="
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium dolorum, rem, necessitatibus repudiandae cum voluptate molestiae eligendi sed laudantium, quam perferendis! Laboriosam, vitae modi? Sapiente quasi veritatis, magnam similique totam saepe odit dolor ullam vel suscipit quos iusto sequi! Accusantium in ratione ad? Delectus eum architecto amet molestiae inventore incidunt?" />
+          {window.location.hash === "#Posts" ? <PostForm handleForm={this.handleForm} handleFormChange={this.handleFormChange} /> : <div></div>}
           <div className={"card-container"}>
             {renderstuff}
           </div>
@@ -77,40 +112,90 @@ class App extends React.Component {
   }
 }
 
-function Post(props) {
-  const { title, description, content, _id } = props.post;
-  return (
-    <div className={"card"} data-id={_id}>
-      <h1>
-        {title}
-      </h1>
-      <h3>
-        {description}
-      </h3>
-      <p>
-        {content}
-      </p>
-    </div>
-  )
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: ["Posts", "About", "Projects"],
+      currentPage: ''
+    }
+  }
+
+  changePage = (page) => {
+    this.props.pageHandler(page);
+    this.setState({
+      currentPage: page,
+    });
+  }
+
+  render() {
+    let renderstuff = [];
+    this.state.pages.forEach(page => {
+      renderstuff.push(<li key={page}><a href={`#${page}`} onClick={() => this.changePage(page)}>{page}</a></li>)
+    });
+
+    return (
+      <ul className={"nav-ul"}>
+        {renderstuff}
+      </ul>
+    );
+  }
+}
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplayed: true,
+    }
+  }
+
+  deletePost = () => {
+    this.setState({
+      isDisplayed: false,
+    })
+  }
+
+  render() {
+    if (!this.state.isDisplayed) return (<div></div>);
+    const { title, description, content, id } = this.props;
+    const onclick = this.props.deletePost || this.deletePost;
+    return (
+      <div className={"card"} data-id={id}>
+        <div className={"card-title"}>
+          <div className={"remove-btn"} onClick={() => onclick(id)}>X</div>
+          <h1>
+            {title}
+          </h1>
+        </div>
+        <h3>
+          {description}
+        </h3>
+        <p className={"card-content"}>
+          {content}
+        </p>
+      </div>
+    )
+  }
 }
 
 function PostForm(props) {
   return (
     <div>
-      <form className={"post-form"}>
+      <form className={"post-form"} onSubmit={(e) => props.handleForm(e)}>
         <label>
           <h3>Title</h3>
-          <input type="text" name="title" />
+          <input type="text" name="title" required onChange={(e) => props.handleFormChange(e)} />
         </label>
         <label>
           <h3>Description</h3>
-          <input type="text" name="description" />
+          <input type="text" name="description" required onChange={(e) => props.handleFormChange(e)} />
         </label>
         <label>
           <h3>Content</h3>
-          <textarea />
+          <textarea name="content" required onChange={(e) => props.handleFormChange(e)} />
         </label>
-        <button onClick={(e) => props.handleForm(e)}>Post</button>
+        <button className="btn-submit" type="submit">Post</button>
       </form>
     </div>
   )
